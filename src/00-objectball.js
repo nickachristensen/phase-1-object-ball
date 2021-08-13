d9dc169202a6147d426fab69c8724755727cb250
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-13 12:37:49 -0400

    first function

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,78 +3,131 @@ const gameObject = {
         'teamName': 'Brooklyn Nets',
         'colors': 'Black, White',
         'players': {
-            'player name': 'Alan Anderson',
-            'number': 0,
-            'shoe': 16,
-            'points': 22,
-            'rebounds': 12,
-            'assists': 12,
-            'steals': 3,
-            'block': 1,
-            'slamDunks': 1,
-        }
-            'player name': 'Reggie Evans',
-            'number': 30,
-            'shoe': 14,
-            'points': 12,
-            'rebounds': 12,
-            'assists': 12,
-            'steals': 12,
-            'block': 12,
-            'slamDunks': 7,
-            
-        }
-        'players': {
-            'player name': 'Brook Lopez',
-            'number': 11,
-            'shoe': 17,
-            'points': 17,
-            'rebounds': 19,
-            'assists': 10,
-            'steals': 3,
-            'block': 1,
-            'slamDunks': 15,
-            
-        }
-        'players': {
-            'player name': 'Mason Plumlee',
-            'number': 1,
-            'shoe': 19,
-            'points': 26,
-            'rebounds': 12,
-            'assists': 6,
-            'steals': 3,
-            'block': 8,
-            'slamDunks': 5,
-            
-        }
-        'players': {
-            'player name': 'Jason Terry',
-            'number': 31,
-            'shoe': 15,
-            'points': 19,
-            'rebounds': 2,
-            'assists': 2,
-            'steals': 4,
-            'block': 11,
-            'slamDunks': 1,
-            
-        }
-    }  
+            'Alan Anderson': {
+                'number': 0,
+                'shoe': 16,
+                'points': 22,
+                'rebounds': 12,
+                'assists': 12,
+                'steals': 3,
+                'block': 1,
+                'slamDunks': 1
+             },
 
-console.log(gameObject)
+            'Reggie Evans' : {
+                'number': 30,
+                'shoe': 14,
+                'points': 12,
+                'rebounds': 12,
+                'assists': 12,
+                'steals': 12,
+                'block': 12,
+                'slamDunks': 7
+            },
+
+            'Brook Lopez' : {
+                'number': 11,
+                'shoe': 17,
+                'points': 17,
+                'rebounds': 19,
+                'assists': 10,
+                'steals': 3,
+                'block': 1,
+                'slamDunks': 15
+            },
+
+        
+            'Mason Plumlee' : {
+                'number': 1,
+                'shoe': 19,
+                'points': 26,
+                'rebounds': 12,
+                'assists': 6,
+                'steals': 3,
+                'block': 8,
+                'slamDunks': 5
+             },
+
+        
+            'Jason Terry' : {
+                'number': 31,
+                'shoe': 15,
+                'points': 19,
+                'rebounds': 2,
+                'assists': 2,
+                'steals': 4,
+                'block': 11,
+                'slamDunks': 1
+            }
+    }
+},
+
+
 
     'away': {
         'teamName': 'Charlotte Hornets',
-        'colors': null,
+        'colors': 'Turquoise, Purple',
         'players': {
-            'number': null,
-            'shoe': null,
-            'points': null,
-            'rebounds': null,
-            'assists': null,
-            'steals': null,
-            'block': null,
-            'slamDunks': null,
+            'Jeff Adrien': {
+                'number': 4,
+                'shoe': 18,
+                'points': 10,
+                'rebounds': 1,
+                'assists': 1,
+                'steals': 2,
+                'block': 7,
+                'slamDunks': 2
+             },
+
+             'Bismack Biyombo': {
+                'number': 0,
+                'shoe': 16,
+                'points': 12,
+                'rebounds': 4,
+                'assists': 7,
+                'steals': 7,
+                'block': 15,
+                'slamDunks': 10
+             },
+
+             'DeSagna Diop': {
+                'number': 2,
+                'shoe': 14,
+                'points': 24,
+                'rebounds': 12,
+                'assists': 12,
+                'steals': 4,
+                'block': 5,
+                'slamDunks': 5
+             },
+
+             'Ben Gordon': {
+                'number': 8,
+                'shoe': 15,
+                'points': 33,
+                'rebounds': 3,
+                'assists': 2,
+                'steals': 1,
+                'block': 1,
+                'slamDunks': 0
+             },
+
+             'Brendan Haywood': {
+                'number': 33,
+                'shoe': 15,
+                'points': 6,
+                'rebounds': 12,
+                'assists': 12,
+                'steals': 22,
+                'block': 5,
+                'slamDunks': 12
+             },
     }
 }
+}
+
+for (let key in gameObject) {
+    let player = gameObject[key];
+    console.log('Jeff Adrien', player, 'number',  key);
+}
+
