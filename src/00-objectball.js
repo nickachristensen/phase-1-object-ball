9bfe5b1a41f8c2d7b770d0ce520003bfaf4227ae
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-13 11:44:07 -0400

    test

---
+++ src/00-objectball.js
@@ -0,0 +1,80 @@
+const gameObject = {
+    'home': {
+        'teamName': 'Brooklyn Nets',
+        'colors': 'Black, White',
+        'players': {
+            'player name': 'Alan Anderson',
+            'number': 0,
+            'shoe': 16,
+            'points': 22,
+            'rebounds': 12,
+            'assists': 12,
+            'steals': 3,
+            'block': 1,
+            'slamDunks': 1,
+        }
+            'player name': 'Reggie Evans',
+            'number': 30,
+            'shoe': 14,
+            'points': 12,
+            'rebounds': 12,
+            'assists': 12,
+            'steals': 12,
+            'block': 12,
+            'slamDunks': 7,
+            
+        }
+        'players': {
+            'player name': 'Brook Lopez',
+            'number': 11,
+            'shoe': 17,
+            'points': 17,
+            'rebounds': 19,
+            'assists': 10,
+            'steals': 3,
+            'block': 1,
+            'slamDunks': 15,
+            
+        }
+        'players': {
+            'player name': 'Mason Plumlee',
+            'number': 1,
+            'shoe': 19,
+            'points': 26,
+            'rebounds': 12,
+            'assists': 6,
+            'steals': 3,
+            'block': 8,
+            'slamDunks': 5,
+            
+        }
+        'players': {
+            'player name': 'Jason Terry',
+            'number': 31,
+            'shoe': 15,
+            'points': 19,
+            'rebounds': 2,
+            'assists': 2,
+            'steals': 4,
+            'block': 11,
+            'slamDunks': 1,
+            
+        }
+    }  
+
+console.log(gameObject)
+
+    'away': {
+        'teamName': 'Charlotte Hornets',
+        'colors': null,
+        'players': {
+            'number': null,
+            'shoe': null,
+            'points': null,
+            'rebounds': null,
+            'assists': null,
+            'steals': null,
+            'block': null,
+            'slamDunks': null,
+    }
+}
